e39f4424a8bf75c67289fd22ddace9918cdbcb3d
BautistaEspinosa <bautistaespinosaroman@gmail.com>
2025-04-22 12:57:53 -0600

    Empezando proeycto

---
+++ src/main/java/com/kinpustan/KinPustanApplication.java
@@ -0,0 +1,13 @@
+package com.kinpustan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinPustanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinPustanApplication.class, args);
+	}
+
+}
